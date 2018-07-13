e50f6a027da7e7b0a3f49008f8ccc5c92477e7f1
jdzle95 <jdzlw95@Lus-MacBook-Pro.local>
2018-07-12 19:38:41 -0700

    Complete

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,16 +1,45 @@
 // TODO: Make sure to make this class a part of the synthesizer package
 // package <package name>;
+package synthesizer;
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T>  extends AbstractBoundedQueue<T> implements Iterable<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
     private int last;
     /* Array for storing the buffer data. */
     private T[] rb;
+
+
+    @Override
+    public Iterator<T> iterator(){
+        return new BufferIterator<T>();
+    }
+
+    public class BufferIterator<T> implements Iterator<T> {
+        private int bufferIndex;
+
+        public BufferIterator() {
+            bufferIndex = first;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return fillCount != capacity;
+        }
+
+        @Override
+        public T next() {
+            bufferIndex = updatePosition(bufferIndex);
+            T temp = (T) rb[bufferIndex];
+            bufferIndex++;
+            return temp;
+        }
+
+    }
 
     /**
      * Create a new ArrayRingBuffer with the given capacity.
@@ -21,6 +50,17 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        this.rb = (T[]) new Object[capacity];
+        this.first = 0;
+        this.last = 0;
+        this.fillCount = 0;
+        this.capacity = capacity;
+    }
+
+
+    private int updatePosition(int index) {
+        if (index == capacity) return 0;
+        return index;
     }
 
     /**
@@ -28,8 +68,16 @@ public class ArrayRingBuffer<T>  {
      * throw new RuntimeException("Ring buffer overflow"). Exceptions
      * covered Monday.
      */
+    @Override
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (isFull()) {
+            throw new RuntimeException("Ring Buffer Overflow");
+        }
+        last = updatePosition(last);
+        rb[last] = x;
+        last++;
+        fillCount++;
     }
 
     /**
@@ -37,15 +85,31 @@ public class ArrayRingBuffer<T>  {
      * throw new RuntimeException("Ring buffer underflow"). Exceptions
      * covered Monday.
      */
+    @Override
     public T dequeue() {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        if (isEmpty()) {
+            throw new RuntimeException("Ring Buffer Underflow");
+        }
+        first = updatePosition(first);
+        T temp = rb[first];
+        rb[first] = null;
+        first++;
+        fillCount--;
+        return temp;
     }
 
     /**
      * Return oldest item, but don't remove it.
      */
+    @Override
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        first = updatePosition(first);
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -8,8 +8,75 @@ import static org.junit.Assert.*;
 
 public class TestArrayRingBuffer {
     @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    public void testEnqueueDequeue() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(5);
+
+        for (int i = 0; i < arb.capacity(); i++) {
+            arb.enqueue(i);
+        }
+
+        for (int i = 0; i < arb.capacity() / 2; i++) {
+            assertEquals((int) arb.dequeue(), i);
+        }
+
+        arb.enqueue(18);
+        assertEquals((int) arb.peek(), 2);
+    }
+
+
+    @Test
+    public void testPeek() {
+        BoundedQueue<Integer> arb = new ArrayRingBuffer<>(10);
+
+        for (int i = 0; i < arb.capacity(); i += 1) {
+            arb.enqueue(i);
+        }
+
+        for (int i = 0; i < 5; i += 1) {
+            arb.dequeue();
+        }
+
+        assertEquals((int) arb.peek(), 5);
+    }
+
+    @Test
+    public void testIsFull() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+
+        for (int i = 0; i < arb.capacity(); i += 1) {
+            arb.enqueue(i);
+        }
+
+        assertTrue(arb.isFull());
+    }
+
+
+    @Test
+    public void testIsEmpty() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+
+        for (int i = 0; i < arb.capacity(); i += 1) {
+            arb.enqueue(i);
+        }
+        for (int i = 0; i < arb.capacity(); i += 1) {
+            arb.dequeue();
+        }
+
+        assertTrue(arb.isEmpty());
+    }
+
+    @Test
+    public void testPeek1() {
+        BoundedQueue<Integer> arb = new ArrayRingBuffer<>(10);
+
+        for (int i = 0; i < arb.capacity(); i += 1) {
+            arb.enqueue(i);
+        }
+        int expected = 0;
+        for (int i : arb) {
+            assertEquals((int) expected, (int) i);
+            expected++;
+        }
     }
 
     /** Calls tests for ArrayRingBuffer. */
